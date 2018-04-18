6ccdba878fb1a9b4671e93abaf83ca7debfb6277
ngx-devtools <aelbore.sg@gmail.com>
2018-04-18 08:22:04 +0800

    add vscode launch

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,17 @@
 const gulp = require('gulp');
 
 const { deleteFolderAsync, copyFiles, watcher } = require('@ngx-devtools/common');
-const { build, onClientFileChanged, vendorBundle } = require('@ngx-devtools/build');
+const { build, onClientFileChanged, vendorBundle, buildAsync } = require('@ngx-devtools/build');
 const { bundle } = require('@ngx-devtools/bundle');
 
 const { serverStart, onServerFileChanged } = require('@ngx-devtools/server');
 
-const buildAsync = async () => await deleteFolderAsync('dist').then(() => build());
 const copyToDist = async () => await copyFiles([ 'src/*.html', 'src/*.js' ], 'dist');
 
 gulp.task('build', () => buildAsync());
 
 gulp.task('default', (done) => {  
-  return deleteFolderAsync('dist')
-    .then(() => Promise.all([ copyToDist(), build() ]))
+  return Promise.all([ copyToDist(), buildAsync() ])
     .then(() => Promise.all([ serverStart(), watcher({ onServerFileChanged, onClientFileChanged }) ]))
 });
 
