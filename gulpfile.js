3260bbb452bda919b7d5959d72c8d8dce07e64eb
aelbore <aelbore@gmail.com>
2018-04-21 00:24:59 +0800

    add gulpfile tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
 const gulp = require('gulp');
 
 const { deleteFolderAsync, copyFiles, watcher } = require('@ngx-devtools/common');
-const { build, onClientFileChanged, vendorBundle, buildAsync } = require('@ngx-devtools/build');
-const { bundle } = require('@ngx-devtools/bundle');
+const { build, onClientFileChanged, vendorBundle, buildAsync, buildProd } = require('@ngx-devtools/build');
 
 const { serverStart, onServerFileChanged } = require('@ngx-devtools/server');
 
 const copyToDist = async () => await copyFiles([ 'src/*.html', 'src/*.js' ], 'dist');
 
 gulp.task('build', () => buildAsync());
+
+gulp.task('bundle', (done) => 
+ Promise.all([ deleteFolderAsync('dist') ])
+  .then(() => buildProd())
+);
 
 gulp.task('default', (done) => {  
   return Promise.all([ copyToDist(), buildAsync() ])
