deb87b530bc13be053661e7fdc5acec5159153b7
aelbore <aelbore@gmail.com>
2018-04-23 17:59:42 +0800

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp');
 
-const { deleteFolderAsync, copyFiles, watcher } = require('@ngx-devtools/common');
+const { deleteFolderAsync, copyFiles, watcher, getFiles } = require('@ngx-devtools/common');
 const { build, onClientFileChanged, vendorBundle, buildAsync, buildProd } = require('@ngx-devtools/build');
 
 const { serverStart, onServerFileChanged } = require('@ngx-devtools/server');
@@ -9,10 +9,7 @@ const copyToDist = async () => await copyFiles([ 'src/*.html', 'src/*.js' ], 'di
 
 gulp.task('build', () => buildAsync());
 
-gulp.task('bundle', (done) => 
- Promise.all([ deleteFolderAsync('dist') ])
-  .then(() => buildProd())
-);
+gulp.task('bundle', (done) => deleteFolderAsync('dist') .then(() => build()));
 
 gulp.task('default', (done) => {  
   return Promise.all([ copyToDist(), buildAsync() ])
