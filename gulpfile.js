28aa7fc4c504873934f6cb6779b5fe7397846c4a
Elbore <1553967@zone1.scb.net>
2018-05-06 16:47:35 +0800

    fix os compatibility

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,23 @@
 const gulp = require('gulp');
 
-const { deleteFolderAsync, copyFiles, watcher } = require('@ngx-devtools/common');
+
+const { deleteFolderAsync, copyFile, getFiles, mkdirp, watcher } = require('@ngx-devtools/common');
 const { build, onClientFileChanged, vendorBundle, buildRxjs } = require('@ngx-devtools/build');
 
 const { serverStart, onServerFileChanged } = require('@ngx-devtools/server');
 
+const copyFiles = () => {  
+  const files = getFiles([ 'src/*.html', 'src/*.js' ])
+    .map(file => file.join(',')).join(',').split(',');
+  mkdirp('dist');
+  return Promise.all(files.map(file => copyFile(file, file.replace('src', 'dist'))));
+};
+
 gulp.task('build', done => build());
 
 gulp.task('default', done => {  
-  return Promise.all([ copyFiles([ 'src/*.html', 'src/*.js' ], 'dist'), build() ])
-    .then(() => Promise.all([ serverStart(), watcher({ onServerFileChanged, onClientFileChanged }) ]))
+  return Promise.all([ copyFiles(), build() ])
+    .then(() => Promise.all([ serverStart(), watcher({ onClientFileChanged }) ]))
 });
 
 gulp.task('vendor.bundle', done => vendorBundle()); 
